9ba244a613b8e50b69ccb02415d48a44030df49e
Patrick <jeng1112@gmail.com>
2026-02-06 13:59:40 +0100

    Update: V1.1 mit sauberem Terminal-Clear und Kammer Soll/Ist Anzeige

--- Panda.py
+++ Panda.py
@@ -5,12 +5,12 @@ import logging
 # ==========================================
 # KONFIGURATION
 # ==========================================
-HOST_IP = "192.168.8.174" 
-PANDA_IP = "192.168.8.142"
+HOST_IP = "192.168.x.xxx" 
+PANDA_IP = "192.168.x.xxx"
 PRINTER_SN = "01P00A123456789"
 ACCESS_CODE = "01P00A12"
-HA_URL = "http://192.168.8.195:8123/api/states/sensor.ks1c_bed_temperature"
-HA_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJmZjg4NzFjOTRiMTc0OTJlYTE4MWVhNDY1YmI5M2JjNiIsImlhdCI6MTc3MDI5OTE1OSwiZXhwIjoyMDg1NjU5MTU5fQ.Biu6Ood1bH-xMBHQnfRFE6h2yiFMWWywTfCnFmji61o"
+HA_URL = "http://192.168.x.xxx:8123/api/states/sensor.ks1c_bed_temperature"
+HA_TOKEN = ""
 # ==========================================
 
 logging.basicConfig(level=logging.CRITICAL)
