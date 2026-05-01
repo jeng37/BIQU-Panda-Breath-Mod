8ca65a54a0cea468107e26e583cb89259b3ca76b
Patrick <jeng1112@gmail.com>
2026-05-01 06:21:57 +0200

    Auto sync: 2026-05-01 06:21:57

--- Panda.py
+++ Panda.py
@@ -24,6 +24,8 @@ global_lock = False  # NEU: Sicherheits-Sperre für alle Modi
 global_heating_state = 20.0
 last_switch_time = 0
 last_stop_command_time = 0
+last_live_log_state = None
+last_live_log_time = 0
 bed_sensor_error = False
 bind_confirmed = False
 bind_warning_shown = False
@@ -627,7 +629,9 @@ def setup_mqtt_discovery():
 async def update_limits_from_ws():
     global panda_ws, bind_confirmed, bind_warning_shown
     global global_heating_state, last_switch_time
-    global last_stop_command_time
+    global last_live_log_state, last_live_log_time
+    global last_stop_command_time  
+
     uri = f"ws://{PANDA_IP}/ws"
 
     while True:
@@ -670,7 +674,7 @@ async def update_limits_from_ws():
 
         # ===== NORMALER WS BETRIEB =====
         try:
-            async with websockets.connect(uri, ping_interval=20) as websocket:
+            async with websockets.connect(uri, ping_interval=None, ping_timeout=None, close_timeout=1) as websocket:
 
                 log_event(f"[WS] Verbunden mit Panda {PANDA_IP}")
                 panda_ws = websocket
@@ -1017,6 +1021,32 @@ async def update_limits_from_ws():
                             fan_state,
                             retain=True
                         )
+
+                        if DEBUG:
+                            now = time.time()
+
+                            current_live_log_state = (
+                                round(bed_ist, 1),
+                                round(target, 1),
+                                round(ist, 1),
+                                global_heating_state > 50,
+                                fan_state,
+                                work_mode_live,
+                                info
+                            )
+
+                            if (
+                                current_live_log_state != last_live_log_state
+                                or (now - last_live_log_time) >= 30
+                            ):
+                                last_live_log_state = current_live_log_state
+                                last_live_log_time = now
+
+                                log_event(
+                                    f"LIVE | Bed:{bed_ist:.1f}°C | Kammer:{target:.1f}/{ist:.1f}°C | "
+                                    f"Heizung:{'AN' if global_heating_state > 50 else 'AUS'} | "
+                                    f"Fan:{fan_state} | Modus:{work_mode_live} | Status:{info}"
+                                )
 
 
                     else:
